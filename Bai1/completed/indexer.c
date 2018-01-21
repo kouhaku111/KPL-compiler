195d3f00326547c604de45847fa0fa723f404c1b
tung <tung110197@gmail.com>
2018-01-21 20:39:01 +0700

    finished check proper noun

--- Bai1/completed/indexer.c
+++ Bai1/completed/indexer.c
@@ -9,6 +9,8 @@ typedef struct data {
 }token;
 
 token stop_token[30]; // toi da co 30 tu cam
+token DTR[30];
+int DTR_range = 0;
 int stop_range = 0;
 
 token hash_token[23][20]; // toi da 20 tu co nghia ung voi cac chu cai dau tu a->z
@@ -35,17 +37,34 @@ void to_lower(char buf[]) {
 int check_stop_token(char buf[]) {   // ton tai thi tra lai 1, 0 trong th nguoc lai
     int i = 0;
 
-    to_lower(buf);
     while(i < stop_range) {
         if(strcmp(stop_token[i++].word, buf) == 0) return 1;
     }
     return 0;
 }
 
-int check_hash_token(char buf[]) {    //  tra ve vi tri tu duoc them vao hoac update trong mang cac ki tu dau 
+int check_DTR(char buf[]) {
+    int i = 0;
+
+    while(i < DTR_range) {
+        if(strcmp(DTR[i++].word, buf) == 0) return 1;
+    }
+    return 0;
+}
+
+int get_num_line(int p, int k) {
+    for(int i = 0; i < 100; i++) 
+        if(hash_token[p][k].lnum[i][0] == '\0') return i;
+}
+
+void push_hash_list(char buf[], int line_no) {    //  tra ve vi tri tu duoc them vao hoac update trong mang cac ki tu dau 
     int i = 0;
     int k = buf[0] - 97;
     int range = num[k];
+    int w, n;
+
+    char s[3];
+    sprintf(s, "%d", line_no);  // chuyen so dong sang string
     
     while(i < num[k]) {
         if(strcmp(hash_token[k][i].word, buf) == 0) {    // neu da ton tai trong day
@@ -61,12 +80,13 @@ int check_hash_token(char buf[]) {    //  tra ve vi tri tu duoc them vao hoac up
         num[k] += 1;
     }
 
-    return i;
-}
+    n = get_num_line(k, i); // lay ra so luong dong
+    for(w = 0; w < n; w++)
+        if(strcmp(hash_token[k][i].lnum[w], s) == 0) 
+            break;
+    if(w == n) strcpy(hash_token[k][i].lnum[w], s);
 
-int get_num_line(int p, int k) {
-    for(int i = 0; i < 100; i++) 
-        if(hash_token[p][k].lnum[i][0] == '\0') return i;
+    return;
 }
 
 void push(int op) {
@@ -74,7 +94,8 @@ void push(int op) {
     char buffer[20];
     int index = 0;
     int j = 0;
-    int line_no = 1; 
+    int line_no = 1;
+    int choice; 
 
     if(op == 1) {
         FILE *f = fopen("stopw.txt", "r");
@@ -110,34 +131,42 @@ void push(int op) {
                         } else buffer[j++] = line[i];   // khong phai la danh tu rieng 
                     }
                     else {  //  neu la ki tu dau tien cua 1 dong
-                        //while(line[i+1] >= 'a' && line[i+1] <= 'z') buffer[j++] = line[i];  // copy
-                        buffer[j++] = line[i];    
+                        while((line[i] >= 'a' && line[i] <= 'z') || (line[i] >= 'A' && line[i] <= 'Z'))
+                            buffer[j++] = line[i++];
+                        buffer[j] = '\0';
+                        j = 0;
+                        //if(strcmp(buffer, "At")) printf("haha\n");
+                        to_lower(buffer);
+                        if(!check_stop_token(buffer)) {
+                            if(!check_DTR(buffer)) {
+                                printf("%s co phai danh tu rieng khong (1 la dung, 0 la sai):", buffer);
+                                scanf("%d", &choice);
+                                switch (choice) {
+                                    case 1:
+                                        strcpy(DTR[DTR_range++].word, buffer);
+                                        break;
+                                    case 0:
+                                        push_hash_list(buffer, line_no);
+                                        break;
+                                    default:
+                                        break;    
+                                }
+                            }
+                            
+                        }
+                        //buffer[j++] = line[i];    
                     }
                 }
                 else {
                     buffer[j] = '\0'; 
                     if(*buffer != '\0') {
-                        if(op == 1) {   // xay dung stop token
-                            strcpy(stop_token[index++].word, buffer);
-                            stop_range += 1;
+                        to_lower(buffer);
+                        if(!check_stop_token(buffer)) {  // neu khong ton tai trong stop_token
+                            push_hash_list(buffer, line_no);
                         }
-                        else if(op == 2) {
-                            if(!check_stop_token(buffer)) {  // neu khong ton tai trong stop_token
-                                int p = buffer[0] - 97;    // lay ra vi tri list dang xet
-                                int k = check_hash_token(buffer);   // lay ra vi tri tu dang xet trong 1 list token
-                                char s[3];
-                                sprintf(s, "%d", line_no);  // chuyen so dong sang string
-                                int n = get_num_line(p, k); // lay ra so luong dong
-                                int w = 0;
-                                for(w = 0; w < n; w++)
-                                    if(strcmp(hash_token[p][k].lnum[w], s) == 0) 
-                                        break;
-                                if(w == n) strcpy(hash_token[p][k].lnum[w], s);
-                            }
-                        }
-                        j = 0;
-                    }        
-                }    
+                    }
+                j = 0;
+                }           
             }
         line_no += 1;      // tang chi so dong len 1        
         }
